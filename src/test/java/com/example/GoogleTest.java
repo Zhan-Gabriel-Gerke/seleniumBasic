6f10290bd3c827eb81929b611cbe36b6cf3d190a
Zhan-Gabriel Gerke <zangerke3@gmail.com>
2025-10-21 12:21:02 +0300

    save

--- src/test/java/com/example/GoogleTest.java
+++ src/test/java/com/example/GoogleTest.java
@@ -1,10 +1,11 @@
 package com.example;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -13,14 +14,27 @@ import org.openqa.selenium.support.ui.*;
 
 import java.time.Duration;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class GoogleTest {
     WebDriver driver = new ChromeDriver();
     WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 
+    @BeforeEach
+    public void setup() {
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+    }
+
+    @AfterEach
+    void finish() {
+        driver.close();
+        driver.quit();
+    }
+
     @Test
     void seleniumTest() {
 
-        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
         driver.findElement(By.id("my-text-id")).sendKeys("Selenium");
         driver.findElement(By.name("my-password")).sendKeys("1234");
         driver.findElement(By.cssSelector("button[type='submit']")).click();
@@ -31,10 +45,35 @@ class GoogleTest {
     }
 
     @Test
+    void selectDropdown_setValue(){
+        Select select = new Select(driver.findElement(By.name("my-select")));
+        select.selectByVisibleText("Two");
+        assertEquals("2", select.getFirstSelectedOption().getAttribute("value"));
+    }
+
+    @Test
+    void radio_choseOne(){
+        WebElement r1 = driver.findElement(By.id("my-radio-1"));
+        WebElement r2 = driver.findElement(By.id("my-radio-2"));
+        r2.click();
+        assertTrue(r2.isSelected());
+        assertFalse(r1.isSelected());
+    }
+
+    @Test
+    void range(){
+        WebElement range = driver.findElement(By.name("my-range"));
+        range.sendKeys(Keys.ARROW_RIGHT, Keys.ARROW_RIGHT, Keys.ARROW_RIGHT);
+        String v = range.getAttribute("value");
+        assertNotNull(v);
+        assertTrue(Integer.parseInt(v)>0);
+    }
+
+    /*@Test
     void titleTetest() {
         driver.get("https://www.google.com");
         String title = driver.getTitle();
         assertTrue(title.toLowerCase().contains("google"));
         driver.quit();
-    }
+    }*/
 }
